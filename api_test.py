6512b3e660f51387145f11e90a4c2fc748fe4b05
Piotr Damer <piotrd@czuby.net>
2020-06-27 16:45:40 +0100

    Preliminary DataFrame

--- api_test.py
+++ api_test.py
@@ -1,4 +1,34 @@
-import yfinance as yf
+#!/usr/bin/env python
+# coding: utf-8
 
-msft = yf.Ticker("MSFT")
-msft.history(period="1y")
+# In[64]:
+
+
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# In[84]:
+
+
+spx = yf.Ticker("SP=F")
+gold = yf.Ticker("GC=F")
+
+spx_gold = pd.DataFrame({
+    "SPX": spx.history(period="10y")["Close"],
+    "Gold": gold.history(period="10y")["Close"]
+},
+    columns=["SPX", "Gold"]
+)
+
+spx_gold = spx_gold.sort_values(by="Date", ascending=False)
+
+print(spx_gold)
+
+
+# In[ ]:
+
+
+get_ipython().system('jupyter nbconvert --to python *.ipynb')
